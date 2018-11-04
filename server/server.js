573362a296c586542c8b9a3f7f61a5d68d08b8eb
Luis <filipe.martins.400@gmail.com>
2018-11-04 18:03:32 +0000

    connect server

--- server/server.js
+++ server/server.js
@@ -1,12 +1,25 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
-const publicPath = path.join(__dirname, '../public');  //dirname global
+const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);  //já e chamado por tras
+var io = socketIO(server);
 
-app.use(express.static(publicPath)); //middleware onde estao fichs pubs html
+app.use(express.static(publicPath));
 
-app.listen(port, () => {
+//websocket is a persistent connection
+io.on('connection', (socket) => { //register event listener. represent user from socket not all ports
+  console.log('Server--> New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('Server--> User was disconnected');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Server is up on ${port}`);
 });

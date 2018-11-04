92441c48f03aaf1d8da295de897b1e8f1043ee3b
Luis <filipe.martins.400@gmail.com>
2018-11-04 15:48:52 +0000

    inicio chat

--- server/server.js
+++ server/server.js
@@ -1,11 +1,11 @@
 const path = require('path');
 const express = require('express');
 
-const publicPath = path.join(__dirname, '../public');
+const publicPath = path.join(__dirname, '../public');  //dirname global
 const port = process.env.PORT || 3000;
 var app = express();
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); //middleware onde estao fichs pubs html
 
 app.listen(port, () => {
   console.log(`Server is up on ${port}`);
